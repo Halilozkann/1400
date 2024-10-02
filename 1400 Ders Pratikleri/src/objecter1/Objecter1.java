65064a9b6b863459b731f632689f543220516c74
Halil Özkan <halilozkan9047@gmail.com>
2024-10-02 15:24:52 +0200

    02.10

---
+++ 1400 Ders Pratikleri/src/objecter1/Objecter1.java
@@ -0,0 +1,29 @@
+package objecter1;
+
+class Bil {
+    private String farge;
+
+    public void visFarge() {
+        System.out.println("Fargen til bilen er " + farge);
+    }
+
+    public void settFarge(String fargen) {
+        farge = fargen;
+    }
+
+    public String hentFarge() {
+        return farge;
+    }
+}
+
+public class Objecter1 {
+    public static void main(String[] args) {
+        Bil nyBil = new Bil();
+//        nyBil.farge = "blå";
+        nyBil.settFarge("blå");
+//        nyBil.visFarge();
+        String bilFargen;
+        bilFargen = nyBil.hentFarge();
+        System.out.println("Fargen til bilen er " + bilFargen);
+    }
+}
